11f3557e8c1d1c5766d461c211d602ca26d78adf
Roshan Shah <rs6927921@gmail.com>
2024-08-05 16:13:28 +0545

    new change

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   <h2>Hello World</h2>
       <h1 className='text-red-500'>Hello World </h1>
      <Test1 name={"roshan"} address={"Bhairahawa"}/>
+     <h2>hi</h2>
     </>
   )
 }
